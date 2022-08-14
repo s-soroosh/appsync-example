9ea3b24ab15c49449272c6a27511981bf08d8778
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-08-14 21:53:56 +0200

    Adjust function return type

--- handler.ts
+++ handler.ts
@@ -1,7 +1,7 @@
 export async function hello(event,context) {
     console.log(`Event:${JSON.stringify((event))}, Context:${JSON.stringify(context)}`)
     return {
-        message: 'Go Serverless v3! Your function executed successfully!',
-        input: event,
+        id:1,
+        title: "title1"
     };
 }
