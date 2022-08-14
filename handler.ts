de01f56f9bb6573b958cdeb5f339052bbcd54e73
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-08-14 21:20:56 +0200

    log given event

--- handler.ts
+++ handler.ts
@@ -1,4 +1,5 @@
 export async function hello(event) {
+    console.log(`Event:${JSON.stringify((event))}`)
     return {
         message: 'Go Serverless v3! Your function executed successfully!',
         input: event,
